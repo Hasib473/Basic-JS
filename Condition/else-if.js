dd30cfbb1cac12e797407ad8e5c8e724f87b4c8d
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-08-02 20:41:35 +0600

    nasted if-else and else if condition

---
+++ Condition/else-if.js
@@ -0,0 +1,15 @@
+// problem holo ulta bondhuo akta sorto dia dilo bollo jodi 18er boro hoi taile cake . jodi 15 er besi hoi taile juice ar jodi 10 er besi hoi taile coclate . ei problem taile kivabe solve korbo
+
+let age=13;
+if (age >=18){
+    console.log("Your are eligible for Cake");
+}
+
+else if(age >=15){
+    console.log("You are eligible for juice");
+
+}
+
+else{
+    console.log("You are eligble for coclate");
+}
---
+++ Condition/nestedCondition.js
@@ -0,0 +1,29 @@
+//  colo ekhon akta problem dekhi nested if else kivabe likhtey hoy
+
+let marks=77;
+
+if(marks >=33 ){
+    console.log("Pass ")
+
+    if (marks>=80){
+        console.log("A+");
+
+    }
+
+    else if( marks >=70){
+        console.log("A");
+    }
+
+    else if(marks >=60){
+        console.log("A-")
+    }
+
+    else {
+        console.log("B")
+    }
+}
+
+else{
+    console.log("you are Fail")
+}
+
