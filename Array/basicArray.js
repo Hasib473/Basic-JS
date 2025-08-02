e09cdd283592f6e29f6970b3b4ef7288ecfb3fae
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-08-02 23:32:49 +0600

    array aktu jhala fala

---
+++ Array/basicArray.js
@@ -0,0 +1,10 @@
+// Array is one kind of Data structue . Its  a special variable that can hold multiple date or many kind of data.
+
+
+// How can we diclare a array?
+
+
+let array=[2,3,false,'food', 5];
+console.log(array);
+console.log(array.length);
+
---
+++ Array/reverseArray.js
@@ -0,0 +1,38 @@
+// using reverse function .
+
+let arr=[2,3,4,5,6];
+arr.reverse();
+console.log(arr);
+
+// acendring order  using sort
+
+let arra=[2,4,5,2,3,5];
+arra.sort((a,b)=> a-b);
+console.log(arra);
+
+// decendring order 
+
+let Array=[50,23,44,34,66];
+Array.sort((a,b)=>b-a);
+console.log(Array);
+
+
+// height numbr ber kora ... Etai aktu loop use korbo kichu mone koro na
+
+
+let bla=[23,43,12,53,55];
+let height=0;
+for(let i of bla){
+
+    if (i>height){
+        height=i;
+    }
+
+}
+console.log(height);
+
+
+
+// array assendring ba decendring korar por amra 2nd height bolo ar last height bolo sob e ber kortey pari
+
+
