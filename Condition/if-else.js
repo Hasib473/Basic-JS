8b7bf2f16bf5d7620b68742f90b2ecb950c9e8f2
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-08-02 20:11:37 +0600

    adding if-else consdtional task

---
+++ Condition/if-else.js
@@ -0,0 +1,20 @@
+// Dhoro ekdin dui bondhu jassilo cake er dokan er pash diye .hotat bondhu1 bondhu2 key bollo aj amar jonmo din amakey tui cake khawa tokhon bondhu2 bollo akta conditon a tor boyos jodi 18 bochor er boro hoy tokhon tui cake khaitey parbi . ei condition programming er vasay kivabey likhey
+
+let age = 19;
+
+if(age > 18){
+    console.log("bondhu suvo jonmodin cake kha")
+}
+
+// okey ekhan e toh dekhlam jodi sorto purno hoy tokhon print korassey kintu jokhon sorto puron hobey na tokhon ki dekhabe . 
+// amra tokhon else condition use kortey pari
+
+let boyos=12;
+if(boyos > 18){
+    console.log("vai tuk cake kha");
+}
+// jodi if er condition mittha hoy tokhon else a jabey
+
+else{
+    console.log("You are not eligible for celebreting birthday");
+}
