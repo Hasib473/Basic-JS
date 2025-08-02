ce0399e2670595e31c144c2328643f3efe4bbc83
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-08-02 21:25:09 +0600

    add here some ternary operation problm

---
+++ Condition/ternary.js
@@ -0,0 +1,40 @@
+// problem : 01-->
+
+let a =9;
+
+let result= a%2===0 ? "a is a even number " : "a is an odd number ";
+
+console.log(result);
+
+
+// problem : 02 -->
+
+let number=-5;
+let result2 = number >0 ? "Possitive" :
+number === 0 ? "nuber is zero ":
+"nuber is negative";
+
+console.log(result2);
+
+
+//problem : 03 -->
+
+let marks=70;
+let result3 = marks>=33 ? "pass" : "Fail";
+console.log(result3);
+
+// problem : 04 -->
+let i=33;
+let j= 44;
+let result4 = i>j ? i+" is bigger then "+j: j+"is bigger then"+i;
+
+console.log(result4);
+
+
+// problem : 5 -->
+
+let temp = 37;
+let abahowa = temp>=35 ? "Turn on the Ac" :
+temp>=30?"Turn on the fan " : "vai kono gorom nai  fan ar ac off kor";
+
+console.log(abahowa);
